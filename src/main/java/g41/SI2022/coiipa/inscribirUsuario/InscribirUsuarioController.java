5b4123bd5ec621aea5683f385c27e47ffd08298f
miermontoto <UO283319@uniovi.es>
2023-02-27 12:27:16 +0100

    init form classes

--- src/main/java/g41/SI2022/coiipa/inscribirUsuario/InscribirUsuarioController.java
+++ src/main/java/g41/SI2022/coiipa/inscribirUsuario/InscribirUsuarioController.java
@@ -1,5 +1,14 @@
 package g41.SI2022.coiipa.inscribirUsuario;
 
 public class InscribirUsuarioController {
-    
+    private InscribirUsuarioModel model;
+    private InscribirUsuarioView view;
+
+    public InscribirUsuarioController(InscribirUsuarioModel m, InscribirUsuarioView v) {
+        this.model = m;
+        this.view = v;
+        this.initView();
+    }
+
+    public void initView() { }
 }
--- src/main/java/g41/SI2022/coiipa/inscribirUsuario/InscribirUsuarioModel.java
+++ src/main/java/g41/SI2022/coiipa/inscribirUsuario/InscribirUsuarioModel.java
@@ -1,5 +1,9 @@
 package g41.SI2022.coiipa.inscribirUsuario;
 
+import g41.SI2022.util.Database;
+
 public class InscribirUsuarioModel {
-    
+
+    private Database db = new Database();
+
 }
