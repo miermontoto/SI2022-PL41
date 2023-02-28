79551f8d3ed28502857bd393a3c1bd0746eb4f03
Rubén Martínez Ginzo <rubenmg2015@gmail.com>
2023-02-28 23:07:18 +0100

    ConsultarCursosView first steps

--- src/main/java/g41/si2022/coiipa/consultar_cursos/ConsultarCursosView.java
+++ src/main/java/g41/si2022/coiipa/consultar_cursos/ConsultarCursosView.java
@@ -1,11 +1,24 @@
 package g41.si2022.coiipa.consultar_cursos;
 
 import g41.si2022.util.SwingMain;
+import g41.si2022.util.Tab;
 
-public class ConsultarCursosView {
+public class ConsultarCursosView extends Tab{
 	
+	private static final long serialVersionUID = 1L;
+
 	public ConsultarCursosView(SwingMain main) {
 		super(main);
-		initialize();
+		this.initialize();
+	}
+	
+	public void initialize() {
+		
+	}
+
+	@Override
+	protected void initController() {
+		// TODO Auto-generated method stub
+		
 	}
 }
