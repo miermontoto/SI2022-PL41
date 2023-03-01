cbdca41f87ca000e6ed499030174c42fd123be76
miermontoto <UO283319@uniovi.es>
2023-03-01 09:22:37 +0100

    rework data to follow db structure, add new sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,18 +3,23 @@ insert into docente (nombre, apellidos, dni, email, telefono, direccion) values
     ('María', 'García', '87654321B', 'mariagarcia@mail.com', '444555666', 'C/ Menor 2'),
     ('Pedro', 'Martínez', '34567890C', 'pedromartinez@mail.com', '777888999', 'C/ Central 3');
 
-insert into curso (nombre, descripcion, estado, start_inscr, end_inscr, plazas, ingresos_estimados, gastos_estimados, docente_id) values
-    ('Introducción a la programación en Python', 'Curso de iniciación a la programación en Python', 'En curso', '2023-03-01', '2023-03-31', 20, 500, 200, 1),
-    ('Programación avanzada en Python', 'Curso avanzado de programación en Python', 'En fase de inscripción', '2023-04-01', '2023-04-30', 15, 750, 250, 2),
-    ('Bases de datos con SQL', 'Curso sobre bases de datos con SQL', 'Cerrado', '2023-05-01', '2023-05-31', 10, 300, 150, 3);
+insert into curso (nombre, descripcion, estado, start_inscr, end_inscr, plazas, start, end, docente_id) values
+    ('Introducción a la programación en Python', 'Curso de iniciación a la programación en Python', 'Finalizado', '2022-07-01', '2022-07-15', 20, '2022-07-20', '2022-07-25', 1),
+    ('Programación avanzada en Python', 'Curso avanzado de programación en Python', 'Finalizado', '2022-08-01', '2022-08-15', 15, '2022-08-20', '2022-08-27', 2),
+    ('Bases de datos con SQL', 'Curso sobre bases de datos con SQL', 'Finalizado', '2022-09-01', '2022-09-15', 10, '2022-09-20', '2022-09-23', 3),
+    ('Desarrollo web con Django', 'Curso sobre desarrollo web con Django', 'Finalizado', '2021-10-01', '2021-10-15', 25, '2021-10-20', '2021-10-27', 4),
+    ('Introducción al machine learning', 'Curso de iniciación al machine learning', 'Finalizado', '2022-03-01', '2022-03-15', 12, '2022-03-20', '2022-03-23', 5),
+    ('Programación orientada a objetos', 'Curso avanzado de programación orientada a objetos en Java', 'Finalizado', '2022-06-01', '2022-06-15', 20, '2022-06-20', '2022-06-27', 6),
+    ('Ciberseguridad', 'Curso sobre ciberseguridad y hacking ético', 'Finalizado', '2022-02-01', '2022-02-15', 15, '2022-02-20', '2022-02-23', 7),
+    ('Desarrollo de videojuegos con Unity', 'Curso sobre desarrollo de videojuegos con Unity', 'Finalizado', '2022-01-01', '2022-01-15', 8, '2022-01-20', '2022-01-23', 9);
 
-insert into alumno (nombre, apellidos, dni, email, telefono, direccion, birth) values
-    ('Juan', 'García', '12345678A', 'juan.garcia@gmail.com', '666555444', 'C/ Mayor, 1', '1990-02-12'),
-    ('María', 'Martínez', '87654321B', 'maria.martinez@gmail.com', '666555333', 'C/ Real, 2', '1992-06-25'),
-    ('Luis', 'Sánchez', '45678912C', 'luis.sanchez@gmail.com', '666555222', 'C/ Nueva, 3', '1995-09-03'),
-    ('Laura', 'Gómez', '12348765D', 'laura.gomez@gmail.com', '666555111', 'C/ La Paz, 4', '1987-11-18'),
-    ('Pablo', 'Rodríguez', '54321678E', 'pablo.rodriguez@gmail.com', '666555000', 'C/ del Carmen, 5', '1999-03-30'),
-    ('Ana', 'Fernández', '87651234F', 'ana.fernandez@gmail.com', '666555999', 'C/ Naranjos, 6', '2001-07-12');
+insert into alumno (nombre, apellidos, email, telefono) values
+    ('Juan', 'García', 'juan.garcia@gmail.com', '666555444'),
+    ('María', 'Martínez', 'maria.martinez@gmail.com', '666555333'),
+    ('Luis', 'Sánchez', 'luis.sanchez@gmail.com', ''),
+    ('Laura', 'Gómez', 'laura.gomez@gmail.com', '666555111'),
+    ('Pablo', 'Rodríguez', 'pablo.rodriguez@gmail.com', '666555000'),
+    ('Ana', 'Fernández', 'ana.fernandez@gmail.com', '666555999');
 
 insert into inscripcion (fecha, coste, estado, curso_id, alumno_id) values
     ('2022-05-15', 5000, 'Pagado', 1, 1),
@@ -36,17 +41,6 @@ insert into inscripcion (fecha, coste, estado, curso_id, alumno_id) values
 insert into docencia (remuneracion, curso_id, docente_id) values
 (120, 1, 1), (90, 4, 4), (70, 5, 5),
 (110, 3, 6), (100, 2, 5), (80, 1, 4);
-
-insert into clase (fecha, location, curso_id) values
-('2022-05-10', 'Aula 2', 1), ('2022-05-17', 'Aula 3', 1),
-('2022-06-10', 'Aula 1', 4), ('2022-06-17', 'Aula 1', 4),
-('2022-07-01', 'Aula 4', 5), ('2022-07-08', 'Aula 4', 5),
-('2022-07-15', 'Aula 2', 3), ('2022-07-22', 'Aula 2', 3),
-('2022-08-08', 'Aula 3', 3), ('2022-08-15', 'Aula 1', 3),
-('2022-08-22', 'Aula 1', 3), ('2022-08-29', 'Aula 2', 3),
-('2022-09-01', 'Aula 1', 1), ('2022-09-08', 'Aula 1', 1),
-('2022-09-15', 'Aula 2', 2), ('2022-09-22', 'Aula 2', 2),
-('2022-09-29', 'Aula 3', 2);
 
 insert into pago (importe, fecha, inscripcion_id) values
 (80, '2022-06-05', 1), (80, '2022-07-10', 1),
