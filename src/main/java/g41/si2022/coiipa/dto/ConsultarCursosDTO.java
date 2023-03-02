07a1d1712dcc421f1cca94df1931ef3929e4c762
miermontoto <UO283319@uniovi.es>
2023-03-02 09:12:29 +0100

    fix dto

--- src/main/java/g41/si2022/coiipa/dto/ConsultarCursosDTO.java
+++ src/main/java/g41/si2022/coiipa/dto/ConsultarCursosDTO.java
@@ -1,17 +1,21 @@
 // El objeto en el que se guarda la query de la base de datos
-	
+
 	// Hacer DTO de las inscripciones
 	// El DTO de cursos lo comparto con Mier
 
 package g41.si2022.coiipa.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class ConsultarCursosDTO {
-	// nombre, estado, plazas, start, end
+
 	private String nombre;
 	private String estado;
 	private String plazas;
-	private String start; 
-	private String end;   
+	private String start;
+	private String end;
 
 	public ConsultarCursosDTO() {}
 
@@ -19,19 +23,7 @@ public class ConsultarCursosDTO {
 		this.nombre = nombre;
 		this.estado = estado;
 		this.plazas = plazas;
-		this.start  = start; 
-		this.end    = end;     
+		this.start  = start;
+		this.end    = end;
 	}
-
-	public String getNombre() { return this.nombre; }
-	public String getEstado() { return this.estado; }
-	public String getPlazas() { return this.plazas; }
-	public String getStartInscr() { return this.start; }
-	public String getEndInscr() { return this.end; }
-
-	public void setNombre(String nombre) { this.nombre = nombre; }
-	public void setEstado(String estado) { this.estado = estado; }
-	public void setPlazas(String plazas) { this.plazas = plazas; }
-	public void setStartInscr(String start) { this.start = start; }
-	public void setEndInscr(String end) { this.end = end; }
 }
