35c3d73e1e1d5d9ee121e82b74efaf44ce971cbf
rubennmg <rubenmg2015@gmail.com>
2023-03-02 10:10:23 +0100

    tables adjusted

--- src/main/java/g41/si2022/coiipa/consultar_cursos/ConsultarCursosView.java
+++ src/main/java/g41/si2022/coiipa/consultar_cursos/ConsultarCursosView.java
@@ -8,6 +8,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.Border;
 
 import g41.si2022.util.SwingMain;
 import g41.si2022.util.Tab;
@@ -45,33 +46,39 @@ public class ConsultarCursosView extends Tab {
 		JPanel panel2 = new JPanel();
 		JLabel lblCursos = new JLabel("Seleccionar curso:");
 		tablaCursos = new JTable();
+		JScrollPane scrCursos = new JScrollPane(tablaCursos);
+
 		tablaCursos.setDefaultEditor(Object.class, null);
 		// Add elements
 		panel2.setLayout(new BorderLayout());
 		panel2.add(lblCursos, BorderLayout.NORTH);
-		panel2.add(tablaCursos, BorderLayout.CENTER);
+		panel2.add(scrCursos, BorderLayout.EAST);
 		centerPanel.add(panel2);
 		
 		// lblInscr + tablaInscr
 		JPanel panel3 = new JPanel();
 		JLabel lblInscr = new JLabel("Inscripciones:");
+		JScrollPane scrInscr = new JScrollPane();
 		tablaInscr = new JTable();
 		tablaInscr.setDefaultEditor(Object.class, null);
 		// Add elements
 		panel3.setLayout(new BorderLayout());
 		panel3.add(lblInscr, BorderLayout.NORTH);
 		panel3.add(tablaInscr, BorderLayout.CENTER);
+		panel3.add(scrInscr, BorderLayout.EAST);
 		centerPanel.add(panel3);
 		
 		// lblBalance + tablaBalance
 		JPanel panel4 = new JPanel();
 		JLabel lblBalance = new JLabel("Balance económico:");
+		JScrollPane scrBalance = new JScrollPane();
 		tablaBalance = new JTable();
 		tablaBalance.setDefaultEditor(Object.class, null);
 		// Add elements
 		panel4.setLayout(new BorderLayout());
 		panel4.add(lblBalance, BorderLayout.NORTH);
 		panel4.add(tablaBalance, BorderLayout.CENTER);
+		panel4.add(scrBalance, BorderLayout.EAST);
 		centerPanel.add(panel4);	
 	}
 
