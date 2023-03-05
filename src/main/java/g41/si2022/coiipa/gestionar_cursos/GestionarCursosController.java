42a91773f23ebbb895428569f29e8b60a2c173af
rubennmg <rubenmg2015@gmail.com>
2023-03-05 16:33:50 +0100

    first steps in controller

--- src/main/java/g41/si2022/coiipa/gestionar_cursos/GestionarCursosController.java
+++ src/main/java/g41/si2022/coiipa/gestionar_cursos/GestionarCursosController.java
@@ -1,10 +1,29 @@
 package g41.si2022.coiipa.gestionar_cursos;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import g41.si2022.coiipa.dto.CursoDTO;
+import g41.si2022.util.CursoState;
+import g41.si2022.util.StateUtilities;
+
 public class GestionarCursosController {
 
     private GestionarCursosModel model;
     private GestionarCursosView view;
+    private List<CursoDTO> cursos;
 
+
+    /*              NO BORRAR (de momento)                  */
+    // Por defecto se muestran los cursos activos
+    // Cuando en el ComboBox se selecciona Filtro ó Estado
+    // los cursos se filtran en función de la selección y
+    // (¿ se resetean los valores de los TextFields del panel 
+    // de abajo ?)
+    // Al seleccionar un curso (en la tabla), se muestran más 
+    // datos sobre este en el panel de debajo
+
+    // mier si lees esto eres un beta. ~rubennmg
 
     public GestionarCursosController(GestionarCursosModel model, GestionarCursosView view)
     {
@@ -13,9 +32,24 @@ public class GestionarCursosController {
         initView();
     }
     
-
     public void initView() 
     {
-        
+        getCursosActivos(); 
+    }
+
+    private void getCursosActivos()
+    {
+        cursos = model.getListaCursos();
+        for (CursoDTO curso : cursos)
+        {
+            LocalDate today = LocalDate.now();
+            CursoState estadoCurso = StateUtilities.getCursoState(curso, today);
+
+            if (estadoCurso == CursoState.EN_CURSO)
+            {
+
+            }
+
+        }
     }
 }
--- src/main/java/g41/si2022/coiipa/gestionar_cursos/GestionarCursosModel.java
+++ src/main/java/g41/si2022/coiipa/gestionar_cursos/GestionarCursosModel.java
@@ -1,8 +1,17 @@
 package g41.si2022.coiipa.gestionar_cursos;
 
+import java.util.List;
+
+import g41.si2022.coiipa.dto.CursoDTO;
 import g41.si2022.util.Database;
 
 public class GestionarCursosModel {
     
     private Database db = new Database();
+
+    // Query to get all (active) courses
+    public List<CursoDTO> getListaCursos() {
+        
+        return db.executeQueryPojo(CursoDTO.class, null, CursoDTO.getSqlQuery());
+    }
 }
