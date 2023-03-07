0232dcc7c9950dba516366f93dce4b68a4f4eb95
UO281932 <UO281932@uniovi.es>
2023-03-07 14:16:03 +0100

    # WARNING: head commit changed in the meantime
    Merge con main

--- src/main/java/g41/si2022/ui/TabsSecretaria.java
+++ src/main/java/g41/si2022/ui/TabsSecretaria.java
@@ -1,6 +1,7 @@
 package g41.si2022.ui;
 
 import g41.si2022.coiipa.consultar_cursos.ConsultarCursosView;
+import g41.si2022.coiipa.insertar_devolucion.InsertarDevolucionView;
 import g41.si2022.coiipa.registrar_pago_alumno.RegistrarPagoAlumnoView;
 import g41.si2022.coiipa.registrar_pago_profesor.RegistrarPagoProfesorView;
 
@@ -12,6 +13,7 @@ public class TabsSecretaria extends TabbedFrame {
         tabs.put("Registrar pagos de alumnos", new RegistrarPagoAlumnoView(main));
         tabs.put("Consultar cursos", new ConsultarCursosView(main));
         tabs.put("Registrar pagos a profesores", new RegistrarPagoProfesorView(main));
+        tabs.put("Cancelar inscripciones", new InsertarDevolucionView(main));
 
         addTabs();
     }
