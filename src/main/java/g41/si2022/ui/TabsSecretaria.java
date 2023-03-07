14046b27964c88c6df7903103650ca8742851ece
miermontoto <UO283319@uniovi.es>
2023-03-07 16:50:04 +0100

    add GestionarCursos to ui

--- src/main/java/g41/si2022/ui/TabsSecretaria.java
+++ src/main/java/g41/si2022/ui/TabsSecretaria.java
@@ -1,6 +1,7 @@
 package g41.si2022.ui;
 
 import g41.si2022.coiipa.consultar_cursos.ConsultarCursosView;
+import g41.si2022.coiipa.gestionar_cursos.GestionarCursosView;
 import g41.si2022.coiipa.insertar_devolucion.InsertarDevolucionView;
 import g41.si2022.coiipa.registrar_pago_alumno.RegistrarPagoAlumnoView;
 import g41.si2022.coiipa.registrar_pago_profesor.RegistrarPagoProfesorView;
@@ -14,6 +15,7 @@ public class TabsSecretaria extends TabbedFrame {
         tabs.put("Consultar cursos", new ConsultarCursosView(main));
         tabs.put("Registrar pagos a profesores", new RegistrarPagoProfesorView(main));
         tabs.put("Cancelar inscripciones", new InsertarDevolucionView(main));
+        tabs.put("Gestionar cursos", new GestionarCursosView(main));
 
         addTabs();
     }
