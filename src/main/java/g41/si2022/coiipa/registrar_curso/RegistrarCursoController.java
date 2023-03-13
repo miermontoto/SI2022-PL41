4d552812b9601fdeb00c55d4155e3c1b0334bdc9
miermontoto <mier@mier.info>
2023-03-13 18:41:11 +0100

    fix stupid var names, new date listeners

--- src/main/java/g41/si2022/coiipa/registrar_curso/RegistrarCursoController.java
+++ src/main/java/g41/si2022/coiipa/registrar_curso/RegistrarCursoController.java
@@ -4,17 +4,26 @@ import java.util.List;
 import java.util.Map;
 import java.awt.KeyboardFocusManager;
 import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.Optional;
 
+import java.awt.event.KeyAdapter;
+
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
+import com.github.lgooddatepicker.optionalusertools.DateChangeListener;
 
 import g41.si2022.dto.ProfesorDTO;
 import g41.si2022.ui.Dialog;
 import g41.si2022.ui.SwingUtil;
 import g41.si2022.util.BetterDatePicker;
+import g41.si2022.util.exception.UnexpectedException;
 
 public class RegistrarCursoController extends g41.si2022.mvc.Controller<RegistrarCursoView, RegistrarCursoModel> {
 
@@ -33,33 +42,32 @@ public class RegistrarCursoController extends g41.si2022.mvc.Controller<Registra
 	@Override
 	public void initNonVolatileData() {
 		// Load the insert curso listener
-		this.getView().getSubmitButton().addActionListener((e) -> SwingUtil.exceptionWrapper(() -> insertCurso()));
-		this.loadDateListeners();
-		this.loadTableListeners();
-		this.loadTextAreaListeners();
-		this.getView().getCosteTextField().addKeyListener(new java.awt.event.KeyAdapter() {
+		getView().getBtnRegistrar().addActionListener((e) -> SwingUtil.exceptionWrapper(() -> insertCurso()));
+		loadDateListeners();
+		loadTableListeners();
+		loadTextAreaListeners();
+		JTextField coste = getView().getTxtCoste();
+		coste.addKeyListener(new KeyAdapter() {
 
 			@Override
 			public void keyPressed (KeyEvent e) {
 				if (!Character.isDigit(e.getKeyChar()) && e.getKeyChar() != '.') {
-					javax.swing.JTextField tf = RegistrarCursoController.this.getView().getCosteTextField();
+					JTextField tf = RegistrarCursoController.this.getView().getTxtCoste();
 					if (tf.getText().length() > 1) {
 						tf.setText(tf.getText().substring(0, tf.getText().length()-1));
-					} else {
-						tf.setText("");
-					}
+					} else tf.setText("");
 				}
 			}
 
 		});
-		this.getView().getCosteTextField().addFocusListener(new java.awt.event.FocusListener() {
+		coste.addFocusListener(new FocusListener() {
 
 			@Override
 			public void focusGained(FocusEvent e) { }
 
 			@Override
 			public void focusLost(FocusEvent e) {
-				javax.swing.JTextField tf = RegistrarCursoController.this.getView().getCosteTextField();
+				JTextField tf = RegistrarCursoController.this.getView().getTxtCoste();
 				if (!Character.isDigit(tf.getText().charAt(0)) && tf.getText().charAt(0) != '.') {
 					tf.setText("");
 				}
@@ -75,9 +83,9 @@ public class RegistrarCursoController extends g41.si2022.mvc.Controller<Registra
 	}
 
 	private void loadTextAreaListeners () {
-		java.awt.event.KeyAdapter ka = new java.awt.event.KeyAdapter () {
+		KeyAdapter ka = new KeyAdapter () {
 			@Override
-			public void keyPressed (KeyEvent e) {
+			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_TAB) {
 					e.consume();
 					KeyboardFocusManager fm = KeyboardFocusManager.getCurrentKeyboardFocusManager();
@@ -85,16 +93,16 @@ public class RegistrarCursoController extends g41.si2022.mvc.Controller<Registra
 				}
 			}
 		};
-		this.getView().getObjetivosDescripcionTextArea().addKeyListener(ka);
-		this.getView().getLocalizacionTextArea().addKeyListener(ka);
+		this.getView().getTxtDescripcion().addKeyListener(ka);
+		this.getView().getTxtLocalizacion().addKeyListener(ka);
 	}
 
-	private void loadTableListeners () {
-		this.getView().getTablaProfesores().getModel().addTableModelListener(new TableModelListener () {
+	private void loadTableListeners() {
+		this.getView().getTableProfesores().getModel().addTableModelListener(new TableModelListener () {
 
 			@Override
 			public void tableChanged(TableModelEvent e) {
-				javax.swing.JTable tabla = RegistrarCursoController.this.getView().getTablaProfesores();
+				JTable tabla = RegistrarCursoController.this.getView().getTableProfesores();
 				RegistrarCursoController.this.profesoresMap
 				.get(tabla.getValueAt(e.getFirstRow(), 0).toString())
 				.setRemuneracion(tabla.getValueAt(e.getFirstRow(), e.getColumn()).toString());
@@ -105,49 +113,58 @@ public class RegistrarCursoController extends g41.si2022.mvc.Controller<Registra
 
 	private void loadDateListeners () {
 		BetterDatePicker
-		inscripcionIni = this.getView().getInscripcionIni(),
-		inscripcionFin = this.getView().getInscripcionFin(),
-		cursoIni = this.getView().getCursoIni(),
-		cursoFin = this.getView().getCursoFin();
-		// Load the Inscripcion Ini DatePicker listener (set Fin to next day)
-		inscripcionIni.addDateChangeListener((e) -> {
-			// Query on #20879 -> Resolved to: Allow but issue a warning.
-			if (inscripcionIni.compareTo(this.getView().getMain().getTodayPicker()) < 0) {
-				Dialog.showWarning("Se está seleccionando una fecha anterior al día de hoy.");
-			}
-			if (inscripcionFin.getDate() == null || inscripcionFin.compareTo(inscripcionIni) <= 0) {
-				inscripcionFin.setDate(e.getNewDate().plusDays(1));
-			}
-		});
-		// Load the Curso Ini DatePicker listener (set Fin to next day)
-		cursoIni.addDateChangeListener((e) -> {
-			if (cursoFin.getDate() == null || cursoFin.compareTo(cursoIni) <= 0) {
-				cursoFin.setDate(e.getNewDate().plusDays(1));
-			}
-			if (inscripcionFin.getDate() == null) {
-				inscripcionFin.setDate(e.getNewDate().minusDays(1));
-			}
-		});
-		// Load the Inscripcion Fin DatePicker listener (set Fin to next day if lower than Ini)
-		inscripcionFin.addDateChangeListener((e) -> {
-			if (cursoIni.getDate() == null) {
-				cursoIni.setDate(e.getNewDate().plusDays(1));
-			}
-			if (inscripcionIni.getDate() == null || inscripcionIni.compareTo(inscripcionFin) >= 0) {
-				inscripcionIni.setDate(e.getNewDate().minusDays(1));
-			}
-		});
-		// Load the Curso Fin DatePicker listener (set Fin to next day if lower than Ini)
-		cursoFin.addDateChangeListener((e) -> {
-			if (cursoIni.getDate() == null || cursoIni.compareTo(cursoFin) >= 0) {
-				cursoIni.setDate(e.getNewDate().minusDays(1));
-			}
-		});
+			inscripcionIni = getView().getDateInscrStart(),
+			inscripcionFin = getView().getDateInscrEnd(),
+			cursoIni = getView().getDateCursoStart(),
+			cursoFin = getView().getDateCursoEnd();
+
+		DateChangeListener inscripcionListener = (e) -> {
+			/*if(inscripcionFin.getDate() == null && inscripcionIni.getDate() != null) {
+				inscripcionFin.setDate(inscripcionIni.getDate().plusDays(1));
+				return;
+			}*/
+
+			if(inscripcionFin.getDate() == null) return;
+
+			int diff = inscripcionFin.compareTo(inscripcionIni);
+			if (diff < 0) {
+				inscripcionFin.setDate(null);
+				Dialog.showError("La fecha de fin de inscripción no puede ser anterior a la de inicio.");
+			} else if (diff == 0)  Dialog.showWarning("La fecha de fin de inscripción es igual a la de inicio.");
+
+			if(cursoIni.getDate() != null)
+				if (cursoIni.compareTo(inscripcionFin) <= 0)
+					Dialog.showWarning("Las fechas de curso y de inscripción se solapan.");
+			};
+
+		DateChangeListener cursoListener = (e) -> {
+			/*if(cursoFin.getDate() == null && cursoIni.getDate() != null) {
+				cursoFin.setDate(cursoIni.getDate().plusDays(1));
+				return;
+			}*/
+
+			if(cursoFin.getDate() == null) return;
+
+			int diff = cursoFin.compareTo(cursoIni);
+			if (diff < 0) {
+				cursoFin.setDate(null);
+				Dialog.showError("La fecha de fin de curso no puede ser anterior a la de inicio.");
+			} else if (diff == 0)  Dialog.showWarning("La fecha de fin de curso es igual a la de inicio.");
+
+			if(inscripcionFin.getDate() != null)
+				if (cursoIni.compareTo(inscripcionFin) <= 0)
+					Dialog.showWarning("Las fechas de curso y de inscripción se solapan.");
+		};
+
+		cursoIni.addDateChangeListener(cursoListener);
+		cursoFin.addDateChangeListener(cursoListener);
+		inscripcionIni.addDateChangeListener(inscripcionListener);
+		inscripcionFin.addDateChangeListener(inscripcionListener);
 	}
 
 	public void getListaProfesores() {
 		this.getModel().getListaProfesores().stream().forEach((x) -> profesoresMap.put(x.getDni(), x));
-		this.getView().getTablaProfesores().setModel(
+		this.getView().getTableProfesores().setModel(
 				SwingUtil.getTableModelFromPojos(
 						this.sup.get(),
 						new String[] { "dni", "nombre", "apellidos", "email", "direccion", "remuneracion" },
@@ -158,27 +175,31 @@ public class RegistrarCursoController extends g41.si2022.mvc.Controller<Registra
 						}
 						)
 				);
-		SwingUtil.autoAdjustColumns(this.getView().getTablaProfesores());
+		SwingUtil.autoAdjustColumns(this.getView().getTableProfesores());
 	}
 
 	public void insertCurso () {
 		Optional<ProfesorDTO> profesorElegido = this.getProfesor();
 		if (!profesorElegido.isPresent()) {
-			throw new g41.si2022.util.exception.UnexpectedException("No se ha seleccionado a ningún docente.");
+			throw new UnexpectedException("No se ha seleccionado a ningún docente.");
 		}
 		this.getModel().insertCurso(
-				this.getView().getNombreCurso(), this.getView().getObjetivosDescripcion(),
-				this.getView().getCoste(),
-				this.getView().getInscripcionIniDate(), this.getView().getInscripcionFinDate(),
-				this.getView().getCursoIniDate(), this.getView().getCursoFinDate(),
-				this.getView().getPlazas(), this.getView().getLocalizacion(),
+				getView().getTxtNombre().getText(),
+				getView().getTxtDescripcion().getText(),
+				getView().getTxtCoste().getText(),
+				getView().getDateInscrStart().getDate().toString(),
+				getView().getDateInscrEnd().getDate().toString(),
+				getView().getDateCursoStart().getDate().toString(),
+				getView().getDateCursoEnd().getDate().toString(),
+				getView().getTxtPlazas().getText(),
+				getView().getTxtLocalizacion().getText(),
 				profesorElegido.get().getId(), // El curso solo tiene un profesor
 				profesorElegido.get().getRemuneracion());
 		Dialog.show("Curso registrado con éxito.");
 	}
 
 	private Optional<ProfesorDTO> getProfesor () {
-		javax.swing.table.TableModel model = this.getView().getTablaProfesores().getModel();
+		TableModel model = this.getView().getTableProfesores().getModel();
 		for (int i = 0 ; i < model.getRowCount() ; i++) {
 			if (model.getValueAt(i, model.getColumnCount()-1) != null) {
 				return Optional.of(this.profesoresMap.get(model.getValueAt(i, 0).toString()));
--- src/main/java/g41/si2022/coiipa/registrar_curso/RegistrarCursoView.java
+++ src/main/java/g41/si2022/coiipa/registrar_curso/RegistrarCursoView.java
@@ -6,6 +6,7 @@ import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 
 import com.github.lgooddatepicker.zinternaltools.JIntegerTextField;
+import lombok.Getter;
 
 import g41.si2022.util.FontType;
 import g41.si2022.util.JLabelFactory;
@@ -21,40 +22,22 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 
+@Getter
 public class RegistrarCursoView extends g41.si2022.ui.Tab {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField nombreCurso;
-	private JTextArea objetivosDescripcion, localizacion;
-	private JIntegerTextField plazas;
-	private BetterDatePicker fechaInscripcionIni, fechaInscripcionFin;
-	private BetterDatePicker fechaCursoIni, fechaCursoFin;
-	private JTextField coste;
-	private JTable profTable;
-	private JButton registrarCurso;
+	private JTextField txtNombre;
+	private JTextArea txtDescripcion, txtLocalizacion;
+	private JIntegerTextField txtPlazas;
+	private BetterDatePicker dateInscrStart, dateInscrEnd;
+	private BetterDatePicker dateCursoStart, dateCursoEnd;
+	private JTextField txtCoste;
+	private JTable tableProfesores;
+	private JButton btnRegistrar;
 
-	public String getNombreCurso () { return this.nombreCurso.getText().trim(); }
-	public String getObjetivosDescripcion () { return this.objetivosDescripcion.getText().trim(); }
-	public JTextArea getObjetivosDescripcionTextArea () { return this.objetivosDescripcion; }
-	public String getPlazas () { return this.plazas.getText().trim(); }
-	public String getInscripcionIniDate () { return this.fechaInscripcionIni.getDate().toString(); }
-	public BetterDatePicker getInscripcionIni () { return this.fechaInscripcionIni; }
-	public String getInscripcionFinDate () { return this.fechaInscripcionFin.getDate().toString(); }
-	public BetterDatePicker getInscripcionFin () { return this.fechaInscripcionFin; }
-	public String getCursoIniDate () { return this.fechaCursoIni.getDate().toString(); }
-	public BetterDatePicker getCursoIni () { return this.fechaCursoIni; }
-	public String getCursoFinDate () { return this.fechaCursoFin.getDate().toString(); }
-	public BetterDatePicker getCursoFin () { return this.fechaCursoFin; }
-	public String getCoste () { return this.coste.getText(); }
-	public JTextField getCosteTextField () { return this.coste; }
-	public JTable getTablaProfesores() { return this.profTable; }
-	public JButton getSubmitButton() { return this.registrarCurso; }
-	public JTextArea getLocalizacionTextArea () { return this.localizacion; }
-	public String getLocalizacion () { return this.localizacion.getText(); }
-
-	public void setNombreCurso (String nombreCurso) { this.nombreCurso.setText(nombreCurso); }
-	public void setObjetivosDescripcion (String objetivosDescripcion) { this.objetivosDescripcion.setText(objetivosDescripcion); }
-	public void setPlazas (int plazas) { this.plazas.setText(String.format("%d", Math.max(0, plazas))); }
+	public void setNombreCurso (String nombreCurso) { this.txtNombre.setText(nombreCurso); }
+	public void setObjetivosDescripcion (String objetivosDescripcion) { this.txtDescripcion.setText(objetivosDescripcion); }
+	public void setPlazas (int plazas) { this.txtPlazas.setText(String.format("%d", Math.max(0, plazas))); }
 
 	public RegistrarCursoView (g41.si2022.ui.SwingMain main) {
 		super(main, RegistrarCursoModel.class, RegistrarCursoView.class, RegistrarCursoController.class);
@@ -92,7 +75,7 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 				right.gridx = 1;
 				right.gridy = 0;
 				right.weighty = 1;
-				centerPanel.add(this.nombreCurso = new JTextField(), right);
+				centerPanel.add(this.txtNombre = new JTextField(), right);
 			}
 		} { // Objetivos y Descripcion
 			{ // Label
@@ -102,10 +85,10 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 			} { // Input
 				right.gridy = 1;
 				right.weighty = 2;
-				this.objetivosDescripcion = new JTextArea();
-				this.objetivosDescripcion.setLineWrap(true);
-				this.objetivosDescripcion.setRows(5);
-				centerPanel.add(this.objetivosDescripcion, right);
+				this.txtDescripcion = new JTextArea();
+				this.txtDescripcion.setLineWrap(true);
+				this.txtDescripcion.setRows(5);
+				centerPanel.add(this.txtDescripcion, right);
 			}
 		} { // Plazas
 			{ // Label
@@ -114,7 +97,7 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 				centerPanel.add(new JLabel("Plazas:"), left);
 			} { // Input
 				right.gridy = 2;
-				centerPanel.add(this.plazas = new JIntegerTextField(), right);
+				centerPanel.add(this.txtPlazas = new JIntegerTextField(), right);
 			}
 		} { // Inscripcion
 			{ // Label
@@ -129,7 +112,7 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 					JPanel panelInscDesde = new JPanel(new BorderLayout());
 					panelInscDesde.add(new JLabel("Desde"), BorderLayout.WEST);
 					panelInscDesde.add(new JPanel(), BorderLayout.CENTER);
-					panelInscDesde.add(this.fechaInscripcionIni = new BetterDatePicker(), BorderLayout.EAST);
+					panelInscDesde.add(this.dateInscrStart = new BetterDatePicker(), BorderLayout.EAST);
 					panelInscripciones.add(panelInscDesde, BorderLayout.WEST);
 				} {
 					panelInscripciones.add(new JPanel(), BorderLayout.CENTER);
@@ -137,7 +120,7 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 					JPanel panelInscHasta = new JPanel(new BorderLayout());
 					panelInscHasta.add(new JLabel("Hasta"), BorderLayout.WEST);
 					panelInscHasta.add(new JPanel(), BorderLayout.CENTER);
-					panelInscHasta.add(this.fechaInscripcionFin = new BetterDatePicker(), BorderLayout.EAST);
+					panelInscHasta.add(this.dateInscrEnd = new BetterDatePicker(), BorderLayout.EAST);
 					panelInscripciones.add(panelInscHasta, BorderLayout.EAST);
 				}
 			}
@@ -154,7 +137,7 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 					JPanel panelInscDesde = new JPanel(new BorderLayout());
 					panelInscDesde.add(new JLabel("Desde"), BorderLayout.WEST);
 					panelInscDesde.add(new JPanel(), BorderLayout.CENTER);
-					panelInscDesde.add(this.fechaCursoIni = new BetterDatePicker(), BorderLayout.EAST);
+					panelInscDesde.add(this.dateCursoStart = new BetterDatePicker(), BorderLayout.EAST);
 					panelInscripciones.add(panelInscDesde, BorderLayout.WEST);
 				} {
 					panelInscripciones.add(new JPanel(), BorderLayout.CENTER);
@@ -162,7 +145,7 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 					JPanel panelInscHasta = new JPanel(new BorderLayout());
 					panelInscHasta.add(new JLabel("Hasta"), BorderLayout.WEST);
 					panelInscHasta.add(new JPanel(), BorderLayout.CENTER);
-					panelInscHasta.add(this.fechaCursoFin = new BetterDatePicker(), BorderLayout.EAST);
+					panelInscHasta.add(this.dateCursoEnd = new BetterDatePicker(), BorderLayout.EAST);
 					panelInscripciones.add(panelInscHasta, BorderLayout.EAST);
 				}
 			}
@@ -173,10 +156,10 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 			} { // Input
 				right.gridy = 5;
 				right.fill = GridBagConstraints.BOTH;
-				this.localizacion = new JTextArea();
-				this.localizacion.setLineWrap(true);
-				this.localizacion.setRows(2);
-				centerPanel.add(this.localizacion, right);
+				this.txtLocalizacion = new JTextArea();
+				this.txtLocalizacion.setLineWrap(true);
+				this.txtLocalizacion.setRows(2);
+				centerPanel.add(this.txtLocalizacion, right);
 			}
 		} { // Coste
 			{ // Label
@@ -185,7 +168,7 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 			} { // Input
 				right.gridy = 6;
 				right.fill = GridBagConstraints.BOTH;
-				centerPanel.add(this.coste = new JTextField(), right);
+				centerPanel.add(this.txtCoste = new JTextField(), right);
 			}
 		}
 
@@ -199,17 +182,17 @@ public class RegistrarCursoView extends g41.si2022.ui.Tab {
 				this.getWidth(), 150
 		));
 
-		this.profTable = new JTable();
-		this.profTable.setName("Profesor:");
-		this.profTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.tableProfesores = new JTable();
+		this.tableProfesores.setName("Profesor:");
+		this.tableProfesores.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		// this.profTable.setDefaultEditor(Object.class, null); // No editable
 
-		sp.setViewportView(this.profTable);
+		sp.setViewportView(this.tableProfesores);
 		bottomPane.add(sp, BorderLayout.CENTER);
 
-		this.registrarCurso = new JButton();
-		this.registrarCurso.setText("Registrar Curso");
-		bottomPane.add(this.registrarCurso, BorderLayout.SOUTH);
+		this.btnRegistrar = new JButton();
+		this.btnRegistrar.setText("Registrar Curso");
+		bottomPane.add(this.btnRegistrar, BorderLayout.SOUTH);
 
 		this.add(bottomPane, BorderLayout.SOUTH);
 		add(JLabelFactory.getLabel(FontType.title, "Registrar Curso"), BorderLayout.NORTH);
