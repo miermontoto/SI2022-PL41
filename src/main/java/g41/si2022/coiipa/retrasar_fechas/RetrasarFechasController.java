e5d324e759e709a2b1d434a0ca4b3db9c68b1b00
UO281932 <UO281932@uniovi.es>
2023-03-21 14:51:50 +0100

    Ya funciona tabla en retrasar fechas

--- src/main/java/g41/si2022/coiipa/retrasar_fechas/RetrasarFechasController.java
+++ src/main/java/g41/si2022/coiipa/retrasar_fechas/RetrasarFechasController.java
@@ -1,9 +1,12 @@
 package g41.si2022.coiipa.retrasar_fechas;
 
-import g41.si2022.coiipa.registrar_pago_alumno.RegistrarPagoAlumnoModel;
-import g41.si2022.coiipa.registrar_pago_alumno.RegistrarPagoAlumnoView;
+import java.util.List;
+
+import g41.si2022.dto.CursoDTO;
+import g41.si2022.ui.SwingUtil;
 
 public class RetrasarFechasController extends g41.si2022.mvc.Controller<RetrasarFechasView, RetrasarFechasModel> {
+
 
 	public RetrasarFechasController(RetrasarFechasView myTab, RetrasarFechasModel myModel) {
 		super(myTab, myModel);
@@ -19,6 +22,23 @@ public class RetrasarFechasController extends g41.si2022.mvc.Controller<Retrasar
 	@Override
 	public void initVolatileData() {
 		// TODO Auto-generated method stub
+		this.updateTables();
+		
+	}
+	
+	
+	public void updateTables() {
+		
+		List <CursoDTO> listacursos = this.getModel().getCursos();
+		
+		this.getView().getCursos().setModel(
+				SwingUtil.getTableModelFromPojos(
+						listacursos,
+						new String[] { "nombre", "start_inscr", "end_inscr", "start", "end"},
+						new String[] { "Nombre", "Inicio Inscripciones", "Fin Inscripciones", "Inicio Curso", "Fin Curso"},
+						null
+						)
+				);
 		
 	}
 
--- src/main/java/g41/si2022/coiipa/retrasar_fechas/RetrasarFechasModel.java
+++ src/main/java/g41/si2022/coiipa/retrasar_fechas/RetrasarFechasModel.java
@@ -1,6 +1,24 @@
 package g41.si2022.coiipa.retrasar_fechas;
 
+import g41.si2022.dto.CursoDTO;
+
 public class RetrasarFechasModel extends g41.si2022.mvc.Model{
+	
+	public java.util.List<CursoDTO> getCursos () {
+		String sql =
+				"SELECT curso.id "
+				+ " ,curso.nombre "
+				+ " ,curso.start_inscr "
+				+ " ,curso.end_inscr "
+				+ " ,curso.start "
+				+ " ,curso.end"
+				+ " FROM curso "
+				+ " LEFT JOIN docencia ON curso.id = docencia.curso_id " // Change for INNER JOIN. To do this, INSERT INTO docencia when creating a new course
+				+ " LEFT JOIN inscripcion ON inscripcion.curso_id = curso.id "
+				+ " LEFT JOIN pago ON pago.inscripcion_id = inscripcion.id "
+				+ " GROUP BY (curso.id)";
+		return this.getDatabase().executeQueryPojo(CursoDTO.class, sql);
+	}
 	
 	public void retrasarFechaCurso(String fecha, String idCurso) {
 		
