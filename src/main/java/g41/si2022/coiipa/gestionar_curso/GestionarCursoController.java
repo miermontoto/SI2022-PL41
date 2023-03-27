45aadff05ea5a3f768d93ae7153e25f9605fa0df
Manu <uo281932@uniovi.es>
2023-03-27 16:56:29 +0200

    Mejorada interfaz y métodos para procesar la selección

--- src/main/java/g41/si2022/coiipa/gestionar_curso/GestionarCursoController.java
+++ src/main/java/g41/si2022/coiipa/gestionar_curso/GestionarCursoController.java
@@ -2,9 +2,11 @@ package g41.si2022.coiipa.gestionar_curso;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.swing.JTable;
+import javax.swing.table.TableModel;
 
 import g41.si2022.dto.CursoDTO;
 import g41.si2022.ui.SwingUtil;
@@ -12,6 +14,11 @@ import g41.si2022.ui.SwingUtil;
 public class GestionarCursoController extends g41.si2022.mvc.Controller<GestionarCursoView, GestionarCursoModel> {
 
 
+	int idCurso;
+	String nombreCurso;
+	String fechaCurso;
+	String fechaInscripciones;
+	
 	public GestionarCursoController(GestionarCursoView myTab, GestionarCursoModel myModel) {
 		super(myTab, myModel);
 		// TODO Auto-generated constructor stub
@@ -31,25 +38,79 @@ public class GestionarCursoController extends g41.si2022.mvc.Controller<Gestiona
 		
 	}
 	
-	int idCurso = 0;
-	
 	public void initThings() {
 		
 		this.getView().getTableInscripciones().addMouseListener( new MouseAdapter()
 		{
 		    public void mousePressed(MouseEvent e)
 		    {
-		        JTable source = (JTable)e.getSource();
-		        int row = source.rowAtPoint( e.getPoint() );
-		        int column = source.columnAtPoint( e.getPoint() );
 
-		        if (! source.isRowSelected(row))
-		        	idCurso = (int) source.getValueAt(row, 0);
+		        handleSelect(); //Que handleSelect() se encargue de todo lo relacionado con esta selección.
 		        
-		        System.out.println(idCurso);
 		        }
 		});
 		
+	}
+	
+	private void handleSelect() { //Función para manejar la fila seleccionada de la tabla
+		
+		
+		TableModel model = this.getView().getTableInscripciones().getModel();
+		
+		//Obtengo los datos de la tabla y los almaceno en variables globales (por si a otros métodos les hacen falta)
+		idCurso = this.getView().getTableInscripciones().getSelectedRow();
+		nombreCurso = this.getView().getTableInscripciones().getModel().getValueAt(idCurso, 1).toString();
+		fechaInscripciones = this.getView().getTableInscripciones().getModel().getValueAt(idCurso, 2).toString();
+		fechaCurso = this.getView().getTableInscripciones().getModel().getValueAt(idCurso, 4).toString();
+
+		//DEBUG
+		
+		System.out.printf("ID: %s, nombre: %s, fecha del curso: %s, fecha de inscripciones: %s \n", idCurso, nombreCurso, fechaCurso, fechaInscripciones);
+
+		
+		if(idCurso == -1) {
+			eraseControls(true);
+			return;
+		}
+		
+		
+		//Set de etiquetas en la IU.
+		this.getView().getLblFechaCurso().setText(fechaCurso);
+		this.getView().getLblInfoNombre().setText(nombreCurso);
+		this.getView().getDatePickerNewDateCurso().setText(fechaCurso);
+		this.getView().getDatePickerNewDateInscripciones().setText(fechaInscripciones);
+		
+		
+		/*String date = model.getValueAt(row, 6).toString();
+		if (date.equals("")) {
+			this.getView().getDatePicker().setDateToToday();
+			setControls(true);
+		} else {
+			this.getView().getDatePicker().setDate(LocalDate.parse(date));
+			setControls(false);
+		}*/
+	}
+	
+	private void setControls(boolean status) {
+		this.getView().getBtnCambiarFechas().setEnabled(status);
+		//this.getView().getDatePicker().setEnabled(status);
+	}
+	
+	private void eraseControls(boolean eliminarAviso) {
+		
+		//Etiquetas de información
+		this.getView().getLblInfoNombre().setText("N/A");
+		this.getView().getLblFechaCurso().setText("N/A");
+		
+		//Datepicker
+		this.getView().getDatePickerNewDateCurso().setText("");
+		this.getView().getDatePickerNewDateInscripciones().setText("");
+		this.getView().getDatePickerNewDateCurso().setEnabled(false);
+		this.getView().getDatePickerNewDateInscripciones().setEnabled(false);
+
+		
+		//if(eliminarAviso) this.getView().getLblError().setText("");
+		setControls(false);
 	}
 	
 	public void updateTables() {
@@ -64,6 +125,8 @@ public class GestionarCursoController extends g41.si2022.mvc.Controller<Gestiona
 						null
 						)
 				);
+		//Ocultamos la columna 0 de la vista.
+		this.getView().getTableInscripciones().removeColumn(this.getView().getTableInscripciones().getColumnModel().getColumn(0));
 		
 	}
 
