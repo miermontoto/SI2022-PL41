b886cf08f036ad69dfb142fbac1f499353674676
Manu <uo281932@uniovi.es>
2023-03-27 17:09:03 +0200

    Merge branch 'retrasarfechas' of https://github.com/miermontoto/SI2022-PL41.git into retrasarfechas Mejorada lógica en el controlador de registro de cursos

--- src/main/java/g41/si2022/coiipa/gestionar_curso/GestionarCursoController.java
+++ src/main/java/g41/si2022/coiipa/gestionar_curso/GestionarCursoController.java
@@ -2,7 +2,9 @@ package g41.si2022.coiipa.gestionar_curso;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.JTable;
@@ -10,6 +12,7 @@ import javax.swing.table.TableModel;
 
 import g41.si2022.dto.CursoDTO;
 import g41.si2022.ui.SwingUtil;
+import g41.si2022.util.Util;
 
 public class GestionarCursoController extends g41.si2022.mvc.Controller<GestionarCursoView, GestionarCursoModel> {
 
@@ -18,7 +21,9 @@ public class GestionarCursoController extends g41.si2022.mvc.Controller<Gestiona
 	String nombreCurso;
 	String fechaCurso;
 	String fechaInscripciones;
-	
+	LocalDate localDateCurso;
+	LocalDate localDateInscripciones;
+
 	public GestionarCursoController(GestionarCursoView myTab, GestionarCursoModel myModel) {
 		super(myTab, myModel);
 		// TODO Auto-generated constructor stub
@@ -27,7 +32,7 @@ public class GestionarCursoController extends g41.si2022.mvc.Controller<Gestiona
 	@Override
 	public void initNonVolatileData() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -35,52 +40,63 @@ public class GestionarCursoController extends g41.si2022.mvc.Controller<Gestiona
 		// TODO Auto-generated method stub
 		this.initThings();
 		this.updateTables();
-		
+
 	}
-	
+
 	public void initThings() {
-		
+
+		eraseControls(true); //Deshabilitamos los controles hasta que ocurra algo en la tabla.
+
+		//Listener de la tabla, para poder detectar los distintos clicks en la tabla
 		this.getView().getTableInscripciones().addMouseListener( new MouseAdapter()
 		{
-		    public void mousePressed(MouseEvent e)
-		    {
+			public void mousePressed(MouseEvent e)
+			{
 
-		        handleSelect(); //Que handleSelect() se encargue de todo lo relacionado con esta selección.
-		        
-		        }
+				handleSelect(); //Que handleSelect() se encargue de todo lo relacionado con esta selección.
+
+			}
 		});
-		
+
 	}
-	
+
 	private void handleSelect() { //Función para manejar la fila seleccionada de la tabla
-		
-		
+
+
 		TableModel model = this.getView().getTableInscripciones().getModel();
-		
+
 		//Obtengo los datos de la tabla y los almaceno en variables globales (por si a otros métodos les hacen falta)
+
 		idCurso = this.getView().getTableInscripciones().getSelectedRow();
 		nombreCurso = this.getView().getTableInscripciones().getModel().getValueAt(idCurso, 1).toString();
 		fechaInscripciones = this.getView().getTableInscripciones().getModel().getValueAt(idCurso, 2).toString();
 		fechaCurso = this.getView().getTableInscripciones().getModel().getValueAt(idCurso, 4).toString();
 
 		//DEBUG
-		
+
 		System.out.printf("ID: %s, nombre: %s, fecha del curso: %s, fecha de inscripciones: %s \n", idCurso, nombreCurso, fechaCurso, fechaInscripciones);
 
-		
-		if(idCurso == -1) {
+		if(idCurso == -1) { //Lo que se ha seleccionado está mal.
 			eraseControls(true);
 			return;
 		}
-		
-		
+
+
 		//Set de etiquetas en la IU.
 		this.getView().getLblFechaCurso().setText(fechaCurso);
 		this.getView().getLblInfoNombre().setText(nombreCurso);
-		this.getView().getDatePickerNewDateCurso().setText(fechaCurso);
-		this.getView().getDatePickerNewDateInscripciones().setText(fechaInscripciones);
-		
-		
+
+		//Conversión a localDate
+		localDateCurso = LocalDate.parse( new SimpleDateFormat("yyyy-MM-dd").format(Util.isoStringToDate(fechaCurso)));
+		localDateInscripciones = LocalDate.parse( new SimpleDateFormat("yyyy-MM-dd").format(Util.isoStringToDate(fechaInscripciones)));
+
+		//Set de fechas en la IU.
+		this.getView().getDatePickerNewDateCurso().setDate(localDateCurso);
+		this.getView().getDatePickerNewDateInscripciones().setDate(localDateInscripciones);
+
+		//Hemos terminado el proceso, habilitamos los controles
+		setControls(true);
+
 		/*String date = model.getValueAt(row, 6).toString();
 		if (date.equals("")) {
 			this.getView().getDatePicker().setDateToToday();
@@ -89,34 +105,42 @@ public class GestionarCursoController extends g41.si2022.mvc.Controller<Gestiona
 			this.getView().getDatePicker().setDate(LocalDate.parse(date));
 			setControls(false);
 		}*/
-	}
-	
-	private void setControls(boolean status) {
-		this.getView().getBtnCambiarFechas().setEnabled(status);
-		//this.getView().getDatePicker().setEnabled(status);
-	}
-	
-	private void eraseControls(boolean eliminarAviso) {
 		
+	}
+
+	private void setControls(boolean status) {
+		
+		//Habilito los datePicker
+		this.getView().getDatePickerNewDateCurso().setEnabled(status);
+		this.getView().getDatePickerNewDateInscripciones().setEnabled(status);
+		//Habilito el botón de cambiar las fechas.
+		this.getView().getBtnCambiarFechas().setEnabled(status);
+	}
+
+	private void eraseControls(boolean eliminarAviso) {
+
 		//Etiquetas de información
 		this.getView().getLblInfoNombre().setText("N/A");
 		this.getView().getLblFechaCurso().setText("N/A");
-		
+
 		//Datepicker
 		this.getView().getDatePickerNewDateCurso().setText("");
 		this.getView().getDatePickerNewDateInscripciones().setText("");
+
+		setControls(false); //Apagamos los controles
+
+		/*
 		this.getView().getDatePickerNewDateCurso().setEnabled(false);
 		this.getView().getDatePickerNewDateInscripciones().setEnabled(false);
+		 */
 
-		
 		//if(eliminarAviso) this.getView().getLblError().setText("");
-		setControls(false);
 	}
-	
+
 	public void updateTables() {
-		
+
 		List <CursoDTO> listacursos = this.getModel().getCursos();
-		
+
 		this.getView().getTableInscripciones().setModel(
 				SwingUtil.getTableModelFromPojos(
 						listacursos,
@@ -127,7 +151,7 @@ public class GestionarCursoController extends g41.si2022.mvc.Controller<Gestiona
 				);
 		//Ocultamos la columna 0 de la vista.
 		this.getView().getTableInscripciones().removeColumn(this.getView().getTableInscripciones().getColumnModel().getColumn(0));
-		
+
 	}
 
 }
